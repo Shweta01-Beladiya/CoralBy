efab1f3751e83c0c9d5633997de66d680e1a23f4
milanbhakhar192 <milan.kalathiyainfotech@gmail.com>
2025-09-29 17:58:44 +0530

    Milan 29-09

---
+++ Fronted/src/Store/Slices/contactusSlice.js
@@ -0,0 +1,58 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios";
+
+const BaseUrl = "http://localhost:9000";
+
+export const createContactUs = createAsyncThunk(
+    "contactUs/create",
+    async (formData, { rejectWithValue }) => {
+        try {
+            const response = await axios.post(`${BaseUrl}/api/createContactUs`, formData);
+            return response.data;
+        } catch (error) {
+            console.log("Axios error:", error.response); // <--- debug
+            return rejectWithValue(
+                error.response?.data?.message || "Something went wrong"
+            );
+        }
+    }
+);
+
+const contactusSlice = createSlice({
+    name: "contactUs",
+    initialState: {
+        loading: false,
+        data: [],
+        error: null,
+        success: false,
+    },
+    reducers: {
+        resetState: (state) => {
+            state.loading = false;
+            state.data = [];
+            state.error = null;
+            state.success = false;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createContactUs.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+                state.success = false;
+            })
+            .addCase(createContactUs.fulfilled, (state, action) => {
+                state.loading = false;
+                state.data = action.payload;
+                state.success = true;
+            })
+            .addCase(createContactUs.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload || "Failed to send message";
+                state.success = false;
+            });
+    }
+});
+
+export const { resetState } = contactusSlice.actions;
+export default contactusSlice.reducer;
