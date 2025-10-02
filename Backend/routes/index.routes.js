8dee467ebac28fdee9dff80a84b2d865730b23a1
Heet Dhameliya <hit.kalathiyainfotech@gmail.com>
2025-10-02 15:08:17 +0530

    current order & order history code

--- Backend/routes/index.routes.js
+++ Backend/routes/index.routes.js
@@ -14,7 +14,7 @@ import { createSubCategory, deleteSubCategoryById, getAllSubCategory, getSubCate
 import { brandFilterController, createBrand, deleteBrand, getAllBrand, getBrandById, getBrandByMainCategory, getSellerBrands, updateBrand } from '../controller/brand.controller.js';
 import { addToWishlist, getWishlist, removeFromWishlist } from '../controller/wishlist.controller.js';
 import { createCoupon, deleteCoupon, getAllCoupon, getCouponById, updateCoupon } from '../controller/coupon.controller.js';
-import { addOrderInstructionsController, cancelMyOrderController, getSellerAllOrdersController, getShippingEstimates, myOrderController, newOrderController, orderSummeryController, updateOrderStatusController, verifyAUPostCodeController } from '../controller/order.controller.js';
+import { addOrderInstructionsController, cancelMyOrderController, getSellerAllOrdersController, getShippingEstimates, myHistoryOrderController, myOrderController, newOrderController, orderSummeryController, updateOrderStatusController, verifyAUPostCodeController } from '../controller/order.controller.js';
 import { createReview, deleteReview, dislikeReview, getProductReviews, likeReview, updateReview } from '../controller/review.controller.js';
 import { addProductBannerController, deleteProductBannerController, getProductBannerController, updateProductBannerController } from '../controller/product.banner.controller.js';
 import { applyJobController, currentJobController, deleteJobApplicationController, getCurrentJobByIdController, getMyJobapplicationsController } from '../controller/job.application.controller.js';
@@ -206,7 +206,8 @@ indexRouter.get("/getSeller", sellerAuth, getSeller)
 
 //order.routes.js
 indexRouter.post("/new/order", UserAuth, newOrderController);
-indexRouter.get("/my/order", UserAuth, myOrderController);
+indexRouter.get("/my/current/order", UserAuth, myOrderController);
+indexRouter.get("/my/history/order", UserAuth, myHistoryOrderController);
 indexRouter.get("/seller/orders", sellerAuth, getSellerAllOrdersController);
 indexRouter.patch("/order/status/:orderId", sellerAuth, updateOrderStatusController);
 indexRouter.post("/cancel/my/order/:orderId", UserAuth, cancelMyOrderController);
